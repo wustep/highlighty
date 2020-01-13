069b42f7a48a0e06e6e657643970227a367940d5
Stephen Wu <wustep@fb.com>
2020-01-13 01:15:50 -0800

    Hilitor: Support className adding, cleanup

--- src/lib/hilitor.js
+++ src/lib/hilitor.js
@@ -2,25 +2,17 @@
  * Hilitor.js
  * Original JavaScript code by Chirp Internet: www.chirp.com.au
  *
- * Modifications by @wustep:
- * - Revised coding conventions, made some functions private.
- * - Removed breakExpRegExp logic, removing bars by default.
+ * Modifications by @wustep to support applying specified classes to many phrase lists.
  */
 
-function Hilitor(id, tag) {
+function Hilitor(id) {
 
   let targetNode = document.getElementById(id) || document.body;
-  let hiliteTag = tag || "MARK";
+  let hiliteTag = "MARK";
   let skipTags = new RegExp("^(?:" + hiliteTag + "|SCRIPT|FORM|SPAN)$");
-  let colors = ["#ff6", "#a0ffff", "#9f9", "#f99", "#f6f"];
-  let wordColor = [];
-  let colorIdx = 0;
   let matchRegExp = "";
   let openLeft = false;
   let openRight = false;
-
-  // Characters to strip from start and end of the input string
-  let endRegExp = new RegExp('^[^\\w]+|[^\\w]+$', "g");
 
   function setMatchType(type) {
     switch (type) {
@@ -41,7 +33,7 @@ function Hilitor(id, tag) {
   };
 
   function setRegex(input, caseSensitive) {
-    input = input.replace(endRegExp, "");
+    input = input.replace(new RegExp('^[^\\w]+|[^\\w]+$', "g"), "");
     if (input) {
       let regex = "(" + input + ")";
       if (!this.openLeft) {
@@ -66,33 +58,25 @@ function Hilitor(id, tag) {
     console.log(setRegex(input, caseSensitive));
   }
 
-  function getRegex() {
-    let retval = matchRegExp.toString();
-    retval = retval.replace(/(^\/(\\b)?|\(|\)|(\\b)?\/i$)/g, "").replace(/\|/g, " ");
-    return retval;
-  };
-
   // Recursively apply word highlighting
-  function hiliteWords(node) {
+  function hiliteWords(node, classes="") {
     if (node === undefined || !node) return;
     if (!matchRegExp) return;
+    console.log(node.nodeName);
     if (skipTags.test(node.nodeName)) return;
 
     if (node.hasChildNodes()) {
-      for(let i = 0; i < node.childNodes.length; i++)
-        hiliteWords(node.childNodes[i]);
+      for (node of node.childNodes) {
+        hiliteWords(node, classes);
+      }
     }
     if (node.nodeType == 3) { // NODE_TEXT
       if ((nv = node.nodeValue) && (regs = matchRegExp.exec(nv))) {
-        if (!wordColor[regs[0].toLowerCase()]) {
-          wordColor[regs[0].toLowerCase()] = colors[colorIdx++ % colors.length];
-        }
-
         let match = document.createElement(hiliteTag);
         match.appendChild(document.createTextNode(regs[0]));
-        match.style.backgroundColor = wordColor[regs[0].toLowerCase()];
-        match.style.color = "#000";
-
+        if (classes.length) {
+          match.className = classes;
+        }
         let after = node.splitText(regs.index);
         after.nodeValue = after.nodeValue.substring(regs[0].length);
         node.parentNode.insertBefore(match, after);
@@ -110,18 +94,6 @@ function Hilitor(id, tag) {
     }
   };
 
-  // Start highlighting at target node
-  this.apply = function(input) {
-    this.remove();
-    if (input === undefined || !(input = input.replace(/(^\s+|\s+$)/g, ""))) {
-      return;
-    }
-    if (setRegex(input)) {
-      hiliteWords(targetNode);
-    }
-    return matchRegExp;
-  };
-
   /*
    * Apply classes to provided phrases list to provided targetNode.
    * markOptions should be { caseSensitive: bool, partialMatch: bool, ... }
@@ -133,7 +105,6 @@ function Hilitor(id, tag) {
       setMatchType("closed");
     }
     setRegexFromPhrases(phrases, !!options.caseSensitive);
-    hiliteWords(targetNode);
-    //hiliteHighlightyPhrases(classes, markOptions);
+    hiliteWords(targetNode, classes);
   }
 }
