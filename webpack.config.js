1182814922f69bca193b8b4f85eb2944f546e564
Patrick Hubbell <phub58@gmail.com>
2023-08-06 17:13:06 -0400

    Update configurations for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,48 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: './src/highlighty.js',
-  devtool: 'inline-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.ts?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
+    entry: {
+        background: path.join(__dirname, "src", "background.js"),
+        highlighty: path.join(__dirname, "src", "highlighty.js"),
+        options: path.join(__dirname, "src", "options.js"),
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/info.html',
+            filename: 'info.html',
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/options.html',
+            filename: 'options.html',
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'src/manifest.json', to: 'manifest.json' },
+                { from: 'src/img', to: 'img' },
+                { from: 'src/lib', to: 'lib' },
+            ],
+        }),
     ],
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
 };
